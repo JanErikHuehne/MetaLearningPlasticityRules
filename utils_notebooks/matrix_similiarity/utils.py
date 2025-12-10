dab2f1c961091f3627d7665de7120afe0b3a2884
Jan Huehne <jan.huehne@tum.de>
2025-12-10 12:11:28 +0000

    Added utils to extract EI and II connectivity

--- utils_notebooks/matrix_similiarity/utils.py
+++ utils_notebooks/matrix_similiarity/utils.py
@@ -5,6 +5,92 @@ import json
 
 
 
+b2.BrianLogger.log_level_error()
+
+def standard_ei_wrapper(weight_dict):
+    seed = weight_dict.get('seed') if  weight_dict.get('seed') else 42
+    b2.start_scope()
+    b2.seed(seed)
+    
+    """
+    We define a mock model 
+    """
+    NE = 800
+    NI = NE // 4
+    
+    neurons = b2.NeuronGroup(NE + NI, model='')
+    # Neuron model
+    Pe = neurons[:NE]
+    Pi = neurons[NE:]
+    con_ei = b2.Synapses(Pe, Pi, model='w : 1')
+    con_ei.connect(p=0.1, condition='i != j')
+   
+    #con_ei.w = np.random.uniform(low=1.0,high=5.0, size=len(con_ei.w))
+    matrix = np.zeros((NE, NI))
+    iis = np.array(con_ei.i)
+    jjs = np.array(con_ei.j)
+
+    matrix[iis, jjs] = 1
+    
+    return matrix
+
+def standard_ii_wrapper(weight_dict):
+    seed = weight_dict.get('seed') if  weight_dict.get('seed') else 42
+    b2.start_scope()
+    b2.seed(seed)
+    
+    """
+    We define a mock model 
+    """
+    NE = 800
+    NI = NE // 4
+    
+    neurons = b2.NeuronGroup(NE + NI, model='')
+    # Neuron model
+    Pe = neurons[:NE]
+    Pi = neurons[NE:]
+    con_ii = b2.Synapses(Pi, Pi, model='w : 1')
+    con_ii.connect(p=0.1, condition='i != j')
+   
+    #con_ei.w = np.random.uniform(low=1.0,high=5.0, size=len(con_ei.w))
+    matrix = np.zeros((NI, NI))
+    iis = np.array(con_ii.i)
+    jjs = np.array(con_ii.j)
+
+    matrix[iis, jjs] = 1
+    
+    return matrix
+
+def standard_ei_wrapper(weight_dict):
+    seed = weight_dict.get('seed') if weight_dict.get('seed') else 42 
+    b2.start_scope()
+    b2.seed(seed)
+    
+    """
+    We define a mock model 
+    """
+    NE = 800
+    NI = NE // 4
+    
+    neurons = b2.NeuronGroup(NE + NI, model='')
+    # Neuron model
+    Pe = neurons[:NE]
+    Pi = neurons[NE:]
+    con_ei = b2.Synapses(Pe, Pi, model='w : 1')
+    con_ii = b2.Synapses(Pi, Pi, model='w : 1')
+    con_ei.connect(p=0.1, condition='i != j')
+   
+    #con_ei.w = np.random.uniform(low=1.0,high=5.0, size=len(con_ei.w))
+    matrix = np.zeros((NE, NI))
+    iis = np.array(con_ei.i)
+    jjs = np.array(con_ei.j)
+
+    matrix[iis, jjs] = 1
+    
+    return matrix
+
+
+
 def standard_ee_wrapper(weight_dict):
     dic = weight_dict['long_run']['weights_ee']
     return extract_weight_matrix(weight_dict=dic, shape=(800,800))
@@ -50,10 +136,13 @@ def extract_weight_matrix(weight_dict, shape=(800,800),  self_connected=True):
 
 
 #with open("/nas/ge84yes/projects/2024OptimizeSTDP/2025_SUMMER_RUNS/wf/v/jjhcxxvfugif_0.json", 'r') as f:
-#    dic =json.load(f)
+#   dic =json.load(f)
 
     
-#ie_matrix  =standard_ie_wrapper(dic)
+#ei_matrix  =standard_ei_wrapper(dic)
+#print(ei_matrix.shape)
+#ii_matrix  =standard_ii_wrapper(dic)
+#print(ii_matrix.shape)
 #print(np.mean(ie_matrix.sum(axis=1)), np.std(ie_matrix.sum(axis=1)))
 #print(np.mean(ie_matrix.sum(axis=0)), np.std(ie_matrix.sum(axis=0)))
 
